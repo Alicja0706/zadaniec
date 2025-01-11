ad4dd62ed30df8f58c2ec4b9df696ac465274546
Alicja0706 <alisziaa07@gmail.com>
2025-01-11 20:08:16 +0100

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>cv</title>
+</head>
+
+<body>
+<div class="container">
+        <img
+            src="zdjecie3.jpg"
+            alt="my photo"
+            width="370"
+            class="photo">
+        >
+        <!--CONTACTS-->
+        <div>
+            <section class="contacts">
+             <h2 class="contacts-title">Kontakt</h2>
+             <div>
+                <li class="contacts-item">
+                    Phone:
+                 <a class="contacts-link" 
+                  href="tel:+48510058183">+48 510 058 183</a>
+                </li>
+                </div>
+                <div>
+                <li class="contacts-item">
+                        Email:
+                 <a class="contacts-link" 
+                    href="mailto:alisziaa07@gmail.com">alisziaa07@gmail.com</a>
+                </li>
+                </div>
+            </section>
+        </div>
+        <!--UMIEJETNOSCI TECHNICZNE-->
+        <div>
+            <section class="skills">
+                <h2 class="skills-title">Umiejetnosci techniczne</h2>
+                <ul class="skills-list">
+                 <li class="skills-item">HTML</li>
+                 <li class="skills-item">CSS</li>
+                  <li class="skills-item">GIT</li>
+                  <li class="skills-item">JavaScript</li>
+                  <li class="skills-item">React</li>
+                  <li class="skills-item">Node.js</li>
+                </ul>
+            </section>
+            <section class="skills">
+                <h2 class="skills-title">Umiejętności miękkie</h2>
+                <ul class="skills-list">
+                    <li class="skills-item">Komunikatywność</li>
+                    <li class="skills-item">Dokłaność</li>
+                    <li class="skills-item">Kreatywność</li>
+                    <li class="skills-item">Pomysłowość</li>
+                </ul>
+            </section>
+        </div>
+    </aside>
+    </div>
+    <aside class="right-side">
+    <!--MAIN CONTAINET-->
+    <div>
+        <div>
+            <section class="bio">
+                <h1 class="bio-title">
+                    Alicja Siok
+                </h1>
+                <p class="bio-occupation">
+                    Informatyk w klasie 4
+                </p>
+                <p class="bio-about">
+                    Jestem Alicja, mam 17 lat i uczę się w 4 klasie o profilu informatycznym
+                </p>
+            </section>
+        </div>
+        <!--PROJEKTY I LINKI-->
+        <div>
+            <section class="projects">
+                   <h2 class="projects-tittle">
+                    Moje projekty</h2>
+                <ol>
+                    <li>
+                      <a 
+                        href="https://www.wp.pl/"
+                        class="projects-links"
+                        > 
+                        wp.pl 
+                       </a>
+                    </li>
+                    <li>
+                        <a 
+                          href="https://www.youtube.com/"
+                          class="projects-links"
+                          > 
+                         youtube.com 
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                           href="https://www.onet.pl/"
+                           class="project-links"
+                           > 
+                           onet.pl
+                        </a>
+                    </li>
+                </ol>     
+             </section>
+        </div>
+        <!--HISTORY-->
+        <div>
+            <ul>
+                <li>interfejs graficzny aplikacji,</li>
+                <li>animacje,</li>
+                <li>przyciski.</li>
+            </ul>
+        </div>
+        <!--FIRMA 1-->
+        <div>
+            <section class="jobs">
+                <h2 class="jobs-title"> Historia zatrudnienia</h2>
+            <h3 class="jobs-occupation"> at Mars, the red planet <span class="accent">freelance</span> </h3>
+            <p class="jobs-time">styczen 2018 - styczen 2020</p>
+            <p></p>
+            <p>Zakres wykonywanych prac</p>
+
+            <ul class="experience">
+                <li class="experience-item">lokalizacja biura projektu,</li>
+                <li class="experience-item">zebranie zespołu,</li>
+                <li class="experience-item">umowy sponsorskie.</li>
+            </ul>
+            </section>
+        </div>
+            <!--WYKSZTAŁCENIE-->
+        <section class="education">
+            <div>
+                <h2 class="education-tittle">Edukacja</h2>
+
+                <h3 class="education-degree">Szkoła Podstawowa <span class="accent"> w Myszyńcu</span></h3>
+                <p class="education-time">wrzesień 2010 - czerwiec 2012</p>
+                <p></p>
+
+                <h3 class="education-degree">Zespół Szkół Powiatowych <span class="accent"> w Myszyńcu</span></h3>
+                <p class="education-time">wrzesień 2012 - czerwiec 2013</p>
+                <p></p>
+            </div>
+        </section>
+    </div>
+    </aside>
+    <!--STRONA LEWA-->
+    <aside class="left-side">
+        <img 
+            src="C:\Users\uczen\Desktop\Klasa 4TB - 24-25\Virtual Studio praca\zdjecie3.jpg"
+            alt="my photo"
+            width="370"
+            class="photo">
+        <!--CONTACTS-->
+        <div>
+            <section class="contacts">
+             <h2 class="contacts-title">Kontakt</h2>
+             <div>
+                <li class="contacts-item">
+                    Phone:
+                 <a class="contacts-link" 
+                  href="tel:+48510058183">+48 510 058 183</a>
+                </li>
+                </div>
+                <div>
+                <li class="contacts-item">
+                        Email:
+                 <a class="contacts-link" 
+                    href="mailto:alisziaa07@gmail.com">alisziaa07@gmail.com</a>
+                </li>
+                </div>
+            </section>
+        </div>
+        <!--UMIEJETNOSCI TECHNICZNE-->
+        <div>
+            <section class="skills">
+                <h2 class="skills-title">Umiejetnosci techniczne</h2>
+                <ul class="skills-list">
+                 <li class="skills-item">HTML</li>
+                 <li class="skills-item">CSS</li>
+                  <li class="skills-item">GIT</li>
+                  <li class="skills-item">JavaScript</li>
+                  <li class="skills-item">React</li>
+                  <li class="skills-item">Node.js</li>
+                </ul>
+            </section>
+            <section class="skills">
+                <h2 class="skills-title">Umiejętności miękkie</h2>
+                <ul class="skills-list">
+                    <li class="skills-item">Komunikatywność</li>
+                    <li class="skills-item">Dokłaność</li>
+                    <li class="skills-item">Kreatywność</li>
+                    <li class="skills-item">Pomysłowość</li>
+                </ul>
+            </section>
+        </div>
+    </aside>
+    </div>
+</div>
+<p class="text red">Umieść kursor</p> 
+<p class="text green">Umieść kursor</p> 
+<p class="text blue">Umieść kursor</p>
+</body>
+</html>
